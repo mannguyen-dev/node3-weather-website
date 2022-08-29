5d49d611a4bf93fba7f22512055a9f5260c45aab
LAPTOP-BHEU8R03\LENOVO <mannguyen.dev@gmail.com>
2022-08-29 15:37:49 +0700

    set up app for heroku

--- public/js/app.js
+++ public/js/app.js
@@ -19,21 +19,19 @@ weatherFrom.addEventListener("submit", (e) => {
     messageOne.textContent = "Loading...";
     messageTwo.textContent = "";
 
-    fetch(`http://localhost:3000/weather?address=${location}`).then(
-        (response) => {
-            response.json().then((data) => {
-                if (data.error) {
-                    messageOne.textContent = data.error;
-                    // console.log(data.error);
-                } else {
-                    // console.log(data.location);
-                    // console.log(data.forecast);
-                    messageOne.textContent = data.location;
-                    messageTwo.textContent = data.forecast;
-                }
-            });
-        }
-    );
+    fetch(`/weather?address=${location}`).then((response) => {
+        response.json().then((data) => {
+            if (data.error) {
+                messageOne.textContent = data.error;
+                // console.log(data.error);
+            } else {
+                // console.log(data.location);
+                // console.log(data.forecast);
+                messageOne.textContent = data.location;
+                messageTwo.textContent = data.forecast;
+            }
+        });
+    });
 
     // console.log(location);
 });
